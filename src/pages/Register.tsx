9a514cc6629b56fb64f026452b37775ad5196253
Ruslan <ruslansorokin234567@gmail.com>
2022-01-04 06:08:17 +0300

    Dialog

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,56 +1,152 @@
-import React from 'react';
-import { makeStyles } from '@mui/styles';
-import { Typography } from '@mui/material';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import Button from '@mui/material/Button';
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import { Typography } from "@mui/material";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import SearchIcon from "@material-ui/icons/Search";
+import PeopleIcon from "@material-ui/icons/PeopleOutline";
+import MessageIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import Button from "@mui/material/Button";
+import CloseIcon from "@mui/icons-material/CloseOutlined";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
 
-const useStyles = makeStyles(theme => ({
-	wrapper: { display: 'flex', height: '100vh' },
-	BlockLeft: { backgroundColor: '#1DA1F2', flex: '0 0 50%' },
-	BlockRight: {
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-		flex: '0 0 50%',
+const useStyles = makeStyles((theme) => ({
+	wrapper: { display: "flex", height: "100vh" },
+	BlockLeft: {
+		backgroundColor: "#71C9F8",
+		flex: "0 0 50%",
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		overflow: "hidden",
+		position: "relative",
 	},
-	BlockRightWrapper: { width: 380 },
+	BlockLeftList: {
+		listStyle: "none",
+		position: "relative",
+		padding: 0,
+		margin: 0,
+		width: 380,
+		"& h6": {
+			display: "flex",
+			alignItems: "center",
+			color: "white",
+			fontWeight: 700,
+			fontSize: 20,
+		},
+	},
+	BlockLeftBigIcon: {
+		position: "absolute",
+		left: "50%",
+		top: "53%",
+		transform: "translate(-50%, -50%)",
+		width: "350%",
+		height: "350%",
+		color: "rgb(29, 161, 243)",
+	},
+	BlockLeftListItem: { marginBottom: 40 },
+	BlockLeftListIcon: { fontSize: 32, marginRight: 15 },
+	BlockRight: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		flex: "0 0 50%",
+	},
+	BlockRightWrapper: {
+		width: 380,
+	},
+	BlockRightTitle: {
+		fontWeight: 700,
+		fontSize: 32,
+		marginBottom: 60,
+		marginTop: 10,
+	},
 	TwitterIcon: {
-		fontSize: 80,
-		color: 'rgb(29, 161, 242)',
+		fontSize: 45,
+		color: "rgb(29, 161, 242)",
 	},
 }));
 
 function Register() {
+	const [open, setOpen] = React.useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+	const handleClose = () => {
+		setOpen(false);
+	};
 	const classes = useStyles();
 
 	return (
 		<div className={classes.wrapper}>
 			<section className={classes.BlockLeft}>
-				<ul>
-					<li>
-						<Typography>Read about what interests you.</Typography>
+				<TwitterIcon color="primary" className={classes.BlockLeftBigIcon} />
+				<ul className={classes.BlockLeftList}>
+					<li className={classes.BlockLeftListItem}>
+						<Typography variant="h6">
+							<SearchIcon className={classes.BlockLeftListIcon} />
+							Read about what interests you.
+						</Typography>
 					</li>
-					<li>
-						<Typography>Find out what the world is talking about.</Typography>
+					<li className={classes.BlockLeftListItem}>
+						<Typography variant="h6">
+							<PeopleIcon className={classes.BlockLeftListIcon} />
+							Find out what the world is talking about.
+						</Typography>
 					</li>
-					<li>
-						<Typography>Join the conversation.</Typography>
+					<li className={classes.BlockLeftListItem}>
+						<Typography variant="h6">
+							<MessageIcon className={classes.BlockLeftListIcon} />
+							Join the conversation.
+						</Typography>
 					</li>
 				</ul>
 			</section>
 			<section className={classes.BlockRight}>
-				<div>
+				<div className={classes.BlockRightWrapper}>
 					<TwitterIcon className={classes.TwitterIcon} />
-					<Typography variant='h4' className={classes.BlockRightWrapper}>
+					<Typography
+						variant="h4"
+						className={classes.BlockRightTitle}
+						gutterBottom
+					>
 						Find out what's happening in the world right now
 					</Typography>
-					<Typography>Join Twitter now!</Typography>
-					<Button variant='contained' color='primary' fullWidth>
+					<br />
+					<Typography>
+						<b>Join Twitter now!</b>
+					</Typography>
+					<Button
+						style={{ marginBottom: 20 }}
+						variant="contained"
+						color="primary"
+						fullWidth
+					>
 						Sign Up
 					</Button>
-					<Button variant='outlined' color='primary' fullWidth>
+					<Button
+						onClick={handleClickOpen}
+						variant="outlined"
+						color="primary"
+						fullWidth
+					>
 						Sign In
 					</Button>
+					<Dialog open={open} onClose={handleClose}>
+						<DialogTitle id="form-dialog-title">
+							<CloseIcon
+								onClick={handleClose}
+								color="secondary"
+								aria-label="close"
+							>
+								<CloseIcon style={{ fontSize: 26 }} />
+							</CloseIcon>
+							Setting search
+						</DialogTitle>
+						<DialogContent>testing...</DialogContent>
+					</Dialog>
 				</div>
 			</section>
 		</div>
--- src/theme.ts
+++ src/theme.ts
@@ -1,34 +1,34 @@
 // @ts-nocheck
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
 
 export const theme = createTheme({
 	typography: {
 		fontFamily: [
-			'system-ui',
-			'-apple-system',
-			'BlinkMacSystemFont',
-			'Segoe UI',
-			'Roboto',
-			'Ubuntu',
-			'Helvetica Neue',
-			'sans-serif',
+			"system-ui",
+			"-apple-system",
+			"BlinkMacSystemFont",
+			"Segoe UI",
+			"Roboto",
+			"Ubuntu",
+			"Helvetica Neue",
+			"sans-serif",
 		],
 	},
 	palette: {
 		primary: {
-			main: 'rgb(29, 161,242)',
-			dark: 'rgb(26, 145,218)',
-			contrastText: '#fff',
+			main: "rgb(29, 161,242)",
+			dark: "rgb(26, 145,218)",
+			contrastText: "#fff",
 		},
 		secondary: {
-			main: 'rgb(26, 145,218)',
+			main: "rgb(26, 145,218)",
 		},
 		error: {
 			main: red.A400,
 		},
 		background: {
-			default: '#fff',
+			default: "#fff",
 		},
 	},
 	shadows: [],
@@ -36,30 +36,31 @@ export const theme = createTheme({
 		MuiButton: {
 			root: {
 				borderRadius: 30,
-				textTransform: 'none',
+				textTransform: "none",
 				fontSize: 16,
 				height: 40,
+				fontWeight: 700,
 			},
 			textPrimary: {
 				paddingLeft: 20,
 				paddingRigth: 20,
 			},
 			outlinedPrimary: {
-				borderColor: 'rgb(29, 161, 243)',
+				borderColor: "rgb(29, 161, 243)",
 			},
 		},
 		MuiFilledInput: {
 			underline: {
-				'&:after': {
-					borderBottomWidth: '2px',
+				"&:after": {
+					borderBottomWidth: "2px",
 				},
-				'&:before': {
-					borderColor: '#000',
-					borderBottomWidth: '2px',
+				"&:before": {
+					borderColor: "#000",
+					borderBottomWidth: "2px",
 				},
 			},
 			input: {
-				backgroundColor: 'rgb(245, 248, 250)',
+				backgroundColor: "rgb(245, 248, 250)",
 			},
 		},
 		MuiDialog: {
@@ -74,15 +75,15 @@ export const theme = createTheme({
 		},
 		MuiDialogTitle: {
 			root: {
-				borderBottom: '1px solid rgb(204, 214, 221)',
+				borderBottom: "1px solid rgb(204, 214, 221)",
 				marginBottom: 10,
-				padding: '10px 15px',
-				'& h2': {
-					display: 'flex',
-					alignItems: 'center',
+				padding: "10px 15px",
+				"& h2": {
+					display: "flex",
+					alignItems: "center",
 					fontWeight: 800,
 				},
-				'& button': {
+				"& button": {
 					padding: 8,
 					marginRigth: 20,
 				},
