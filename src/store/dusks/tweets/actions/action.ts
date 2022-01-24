4f2fc5ea008920b5542ad7a5a6e16243df677262
Ruslan <ruslansorokin234567@gmail.com>
2022-01-24 18:41:13 +0300

    Fetch

--- src/store/dusks/tweets/actions/action.ts
+++ src/store/dusks/tweets/actions/action.ts
@@ -3,6 +3,7 @@ import { TweetsState } from "../contracts/state";
 
 export enum ActionType {
 	SET_TWEETS = "tweets/SET_TWEETS",
+	FETCH_TWEETS = "tweets/FETCH_TWEETS",
 }
 
 export interface SetTweetsActionInterface extends Action<ActionType> {
@@ -10,11 +11,19 @@ export interface SetTweetsActionInterface extends Action<ActionType> {
 	payload: TweetsState["items"];
 }
 
+export interface FetchTweetsActionInterface extends Action<ActionType> {
+	type: ActionType.FETCH_TWEETS;
+}
+
 export const setTweets = (
 	payload: TweetsState["items"]
 ): SetTweetsActionInterface => ({
 	type: ActionType.SET_TWEETS,
 	payload,
+});
+
+export const fetchTweets = (): FetchTweetsActionInterface => ({
+	type: ActionType.FETCH_TWEETS,
 });
 
 export type TweetsActions = SetTweetsActionInterface;
